a9685063a1878ae72ebb2930023308a9bb5086fe
Adriano Amorim <adryanoamorim@hotmail.com>
2023-01-22 10:55:56 -0300

    acrescentando  fonts e estilizacoes

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const img = document.querySelector("#boxImg img");
+
+var imagensSorteio = [
+    {
+        nome: "Cachorro", url: "./assets/img/cachorro.jpg"
+    },
+    {
+        nome: "Gato", url: "./assets/img/gato.jpg"
+    },
+    {
+        nome: "Porco", url: "./assets/img/porco.jpg"
+    },
+]
+
+const sortearNum = numMaxImg => parseInt(Math.random() * numMaxImg)
+
+const sortearImg = ()=>{
+    let positSorteada = sortearNum(3);
+console.log(positSorteada)
+    img.setAttribute("src",`${imagensSorteio[positSorteada].url}`);
+}
+
+sortearImg();
